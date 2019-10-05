a9d6f6638eecaff171e21feda6551c5f6954cc49
DanielBerrioB <daniberrio10@hotmail.com>
2019-10-05 17:18:11 -0500

    Addition

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 const controller = require("./users.controller");
 
-router.get("/user", controller.addUser);
+router.get(path.join(__dirname + "/user", controller.addUser);
 
 module.exports = router;
