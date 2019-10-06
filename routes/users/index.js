ca0d441fab565b06319cb42a5916926bde554b4d
Equipo 2 <equipo2@labredes2.udem.edu.co>
2019-10-05 19:17:52 -0500

    Change

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const path = require("path");
 const controller = require("./users.controller");
 
-router.get(path.join(__dirname + "/user"), controller.addUser);
+router.get("/user", controller.addUser);
 
 module.exports = router;
