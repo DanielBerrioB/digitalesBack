cbe57a3899c89ac41c87175ff675569347066ab1
Equipo 2 <equipo2@labredes2.udem.edu.co>
2019-10-21 16:27:52 -0500

    Nuevos cambios from server

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   pool: {
     connectionLimit: 10,
     host: "localhost",
-    user: "root", //equipo2
-    password: "", //3qu1p02db
+    user: "equipo2", //equipo2
+    password: "3qu1p02db", //3qu1p02db
     database: "equipo2"
   }
 };
